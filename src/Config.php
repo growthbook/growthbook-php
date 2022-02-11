a77c403777bed1dfad24b7647da38dbdf7ecb95b
Jeremy Dorn <jeremy@jeremydorn.com>
2022-02-11 16:14:14 -0600

    Revert Config class

--- src/Config.php
+++ src/Config.php
@@ -2,6 +2,9 @@
 
 namespace Growthbook;
 
+/**
+ * @deprecated
+ */
 class Config
 {
     /** @var bool */
@@ -12,28 +15,22 @@ class Config
     public $enableQueryStringOverride = false;
     /** @var string */
     public $url = null;
-    /** @var array<string,mixed> */
-    public $attributes = [];
-    /** @var array<string,array> */
-    public $features = [];
 
     /**
-     * @param array{enabled?:bool,logger?:\Psr\Log\LoggerInterface,enableQueryStringOverride?:boolean,url?:string,attributes?:array<string,mixed>,features?:array<string,array>} $options
-     */
+     * @param array{enabled?:bool,logger?:\Psr\Log\LoggerInterface,enableQueryStringOverride?:boolean} $options
+    */
     public function __construct(array $options)
     {
         // Warn if any unknown options are passed
-        $knownOptions = ["enabled", "logger", "enableQueryStringOverride", "url", "attributes", "features"];
+        $knownOptions = ["enabled","logger","enableQueryStringOverride","url"];
         $unknownOptions = array_diff(array_keys($options), $knownOptions);
         if (count($unknownOptions)) {
-            trigger_error('Unknown Config options: ' . implode(", ", $unknownOptions), E_USER_NOTICE);
+            trigger_error('Unknown Config options: '.implode(", ", $unknownOptions), E_USER_NOTICE);
         }
 
         $this->enabled = $options["enabled"] ?? true;
         $this->logger = $options["logger"] ?? null;
         $this->enableQueryStringOverride = $options["enableQueryStringOverride"] ?? false;
         $this->url = $options["url"] ?? $_SERVER['REQUEST_URI'] ?? null;
-        $this->attributes = $options["attributes"] ?? [];
-        $this->features = $options["features"] ?? [];
     }
 }
